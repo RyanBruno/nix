26d04b26450db53e180ef12e0661e3c5cbd69267
Ryan Bruno <ryanbruno506@gmail.com>
2025-08-28 08:27:10 -0400

    Mount Zigby device

--- nixos/nixosModules/homeassistant.nix
+++ nixos/nixosModules/homeassistant.nix
@@ -17,7 +17,7 @@
           # Use the host network namespace for all sockets
           "--network=host"
           # Pass devices into the container, so Home Assistant can discover and make use of them
-          #"--device=/dev/ttyACM0:/dev/ttyACM0"
+          "--device=/dev/ttyUSB0:/dev/ttyUSB0"
         ];
       };
     };
